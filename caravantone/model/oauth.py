0472d604a4da8d2ccf03b346165eb01ff249fc62
t-atsumi <t-atsumi@kizasi.jp>
2014-04-18 10:56:21 +0900

    integrate provider class into ProviderType #1

--- caravantone/model/oauth.py
+++ caravantone/model/oauth.py
@@ -6,46 +6,43 @@ from requests_oauthlib import OAuth1Session
 
 secret = None
 
-class Provider(object):
+
+class Provider(Enum):
     """oauth provider
     """
 
-    def __init__(self, name, request_token_uri, authorization_uri, access_token_uri,
-                 callback_uri=None):
-        self.name = name
+    twitter = ('twitter', '', '', 'https://api.twitter.com/oauth/request_token',
+               'https://api.twitter.com/oauth/authenticate', 'https://api.twitter.com/oauth/access_token',
+               'http://192.168.56.101:5000/login/oauth')
+
+    def __init__(self, provider_name, consumer_key, consumer_secret, request_token_uri,
+                 authorization_uri,  access_token_uri,  callback_uri):
+        """constructor
+
+        :param provider_name: name of provider
+        :param consumer_key: consumer key
+        :param consumer_secret: consumer secret
+        :param request_token_uri: uri for request token
+        :param authorization_uri: uri for authorization
+        :param access_token_uri: uri for access token
+        :param callback_uri: callback uri after authorization
+        """
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.provider_name = provider_name
         self.request_token_uri = request_token_uri
         self.authorization_uri = authorization_uri
         self.access_token_uri = access_token_uri
         self.callback_uri = callback_uri
 
-    @property
-    def consumer_key(self):
-        return ''
 
-    @property
-    def consumer_secret(self):
-        return ''
-
-
-class ProviderType(Enum):
-    """ type for oauth provider
-    """
-
-    twitter = Provider(name='twitter',
-                       request_token_uri='https://api.twitter.com/oauth/request_token',
-                       authorization_uri='https://api.twitter.com/oauth/authenticate',
-                       access_token_uri='https://api.twitter.com/oauth/access_token',
-                       callback_uri='http://192.168.56.101:5000/login/oauth')
-
-
-def generate_authorization_url(provider_type):
+def generate_authorization_url(provider):
     """generate url for authorization
 
-    :param ProviderType provider_type: type of provider
+    :param ProviderType provider: type of provider
     :rtype: (str, dict)
     :return: (url for authorization, request token)
     """
-    provider = provider_type.value
     session = OAuth1Session(provider.consumer_key,
                             client_secret=provider.consumer_secret,
                             callback_uri=provider.callback_uri)
@@ -54,15 +51,14 @@ def generate_authorization_url(provider_type):
     return session.authorization_url(provider.authorization_uri, res.get('oauth_token')), res
 
 
-def authorize_access(provider_type, authorization_response_url):
+def authorize_access(provider, authorization_response_url):
     """authorize access to provider
 
-    :param ProviderType provider_type: type of provider
+    :param ProviderType provider: type of provider
     :param str authorization_response_url: url from authorization request
     :rtype: dict
     :return: access token
     """
-    provider = provider_type.value
     session = OAuth1Session(provider.consumer_key,
                             client_secret=provider.consumer_secret)
     res = session.parse_authorization_response(authorization_response_url)
--- caravantone/view/login.py
+++ caravantone/view/login.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from flask import redirect, request
-from caravantone.model.oauth import generate_authorization_url, authorize_access, ProviderType
+from caravantone.model.oauth import generate_authorization_url, authorize_access, Provider
 
 
 def twitter():
-    return redirect(generate_authorization_url(ProviderType.twitter)[0])
+    return redirect(generate_authorization_url(Provider.twitter)[0])
+
 
 def oauth():
-    tokens = authorize_access(ProviderType.twitter, request.url)
+    tokens = authorize_access(Provider.twitter, request.url)
     return ','.join([tokens.get('oauth_token'), tokens.get('oauth_token_secret')])
+
 
 def configure(app):
     app.route('/login/twitter')(twitter)
